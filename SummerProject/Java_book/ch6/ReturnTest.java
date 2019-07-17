b5af4718e2eaaa91199437b6f5c0d0a3c7867375
정민지 <@DESKTOP-D7J4NCH>
2019-07-18 00:40:18 +0900

    참초형반환 연습문제 ※ d.x와 d2.x의 메모리 주소값은 다르다.

--- SummerProject/Java_book/ch6/ReturnTest.java
+++ SummerProject/Java_book/ch6/ReturnTest.java
@@ -1,5 +1,5 @@
 package ch6;
-
+// 기본형 매개변수와 참조형 매개변수를 동시에 사용한 예제
 class ReturnTest {
 
 	public static void main(String[] args) {
@@ -12,11 +12,11 @@ class ReturnTest {
 		r.add(3,5,result2);
 		System.out.println(result2[0]);
 	}
-	int add(int a, int b) {
+	int add(int a, int b) {  // 기본형 매개변수
 		return a + b;
 	}
 	
-	void add(int a, int b, int [] result) {
+	void add(int a, int b, int [] result) {  // 기본형 매개변수 , 참조형 매개변수
 		result[0] = a + b;
 	}
 
